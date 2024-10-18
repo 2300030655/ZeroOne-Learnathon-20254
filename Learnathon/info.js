66a308c1cbafab47ee3d71eaed6feb01eb702aa2
2300030655 <162719854+2300030655@users.noreply.github.com>
2024-10-18 12:54:26 +0530

    Add files via upload
    2300030655_Akshaya

---
+++ Learnathon/info.js
@@ -0,0 +1,7 @@
+let name = "Akshaya";  
+let id = 2300030655;         
+let branch = "Computer Science";  
+
+console.log("Name: " + name);
+console.log("ID: " + id);
+console.log("Branch: " + branch);
---
+++ Learnathon/sum.js
@@ -0,0 +1,16 @@
+const add =(a,b)=>{
+    return a+b;
+}
+const mul =(a,b)=>{
+    return a*b;
+}
+const div =(a,b)=> {
+    return a/b;
+}
+const sub =(a,b)=> {
+    return a-b;
+}
+console.log(add(4,6));
+console.log(sub(4,6));
+console.log(mul(4,6));
+console.log(div(4,6));
